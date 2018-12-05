ddac5c39787ecc4b688097c4b7053edd517c92b1
Briana Gude <briana.gude@gmail.com>
2018-12-05 16:25:13 -0500

    fixed my carrierwave fix haha

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,10 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
+    region: "us-east-2",
     provider:              'AWS',                        # required
     aws_access_key_id:     Rails.application.credentials[Rails.env.to_sym][:aws][:key],
     aws_secret_access_key: Rails.application.credentials[Rails.env.to_sym][:aws][:secret],
   }
   config.fog_directory  = Rails.application.credentials[Rails.env.to_sym][:aws][:bucket]
-
-  region: "us-east-2"
 end
