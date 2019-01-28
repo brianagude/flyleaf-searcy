6d102da3a8470acafc26f263e64d52139a0e9a2d
Briana Gude <briana.gude@gmail.com>
2019-01-27 23:57:03 -0500

    edited header

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -5,11 +5,11 @@ const menu = document.querySelector("section.menu");
 $(button).click(function() {
   $(menu).toggleClass("show-menu");
 
-  if (menu.classList.contains("show-menu")) {
-    button.innerHTML = `<img src="assets/menu.svg"> `;
-  } else {
-    button.innerHTML = `<img src="assets/x.svg"> `;
-  }
+  // if (menu.classList.contains("show-menu")) {
+  //   button.innerHTML = `<img src="assets/menu.svg"> `;
+  // } else {
+  //   button.innerHTML = `<img src="assets/x.svg"> `;
+  // }
 
   return false;
 });
